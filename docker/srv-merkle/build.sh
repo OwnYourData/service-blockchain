5286a1e8438f41e2704e23c13d5a4d1205789a52
Christoph Fabianek <christoph.fabianek@gmail.com>
2019-11-12 16:07:33 +0100

    fix security vulnerability of 'loofah'

--- docker/srv-merkle/build.sh
+++ docker/srv-merkle/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+gem build merkle-hash-tree/merkle-hash-tree.gemspec
 docker build -t oydeu/srv-merkle .
--- docker/srv-merkle/merkle.rb
+++ docker/srv-merkle/merkle.rb
@@ -12,7 +12,7 @@ config = JSON.parse(config_raw)
 
 def defaultHeaders(token)
   { 
-  	'Accept' => '*/*',
+    'Accept' => '*/*',
     'Content-Type' => 'application/json',
     'Authorization' => 'Bearer ' + token
   }
@@ -36,7 +36,7 @@ end
 def setupApp(pia_url, app_key, app_secret)
   token = getToken(pia_url, app_key, app_secret)
   { 
-  	"pia_url"    => pia_url,
+    "pia_url"    => pia_url,
     "app_key"    => app_key,
     "app_secret" => app_secret,
     "token"      => token 
@@ -49,7 +49,7 @@ def writeLog(app, message)
   response = HTTParty.post(url,
                            headers: headers,
                            body: { identifier: "oyd.merkle",
-  								   log: message }.to_json)
+                                   log: message }.to_json)
 end
 
 # setup
@@ -57,70 +57,86 @@ id_array = Array.new
 hash_array = Array.new
 mht = MerkleHashTree.new(hash_array, Digest::SHA256)
 ma = setupApp(config["pia_url"], 
-		      config["app_key"],
-		      config["app_secret"])
+       config["app_key"],
+       config["app_secret"])
 # puts "Token: " + ma['token'].to_s
-
-# create merkel record
 headers = defaultHeaders(ma["token"])
-create_merkle_url = config["pia_url"] + '/api/merkles/create'
-response = HTTParty.post(create_merkle_url,
-		    headers: headers,
-		    body: { }.to_json ).parsed_response
-merkle_id = response['id']
 
 # get list of new and updated items
 data_url = config["pia_url"] + '/api/items/merkle'
-response = HTTParty.get(data_url,
-						headers: headers).parsed_response
-
+items = HTTParty.get(data_url,
+                        headers: headers).parsed_response
 # log entry
-writeLog(ma, 'started hashing ' + response.count.to_s + ' items')
+writeLog(ma, 'start: ' + items.count.to_s + ' items')
 
-# process each item
-response.each do |item|
-	item_id = item['id']
-	item_hash = Digest::SHA256.digest(item['value'])
-	id_array << item_id
-	hash_array << item_hash
-	# puts item_id.to_s + ": " + Base64.encode64(item_hash).rstrip
+if items.count > 0
+  # create merkel record
+  create_merkle_url = config["pia_url"] + '/api/merkles/create'
+  response = HTTParty.post(create_merkle_url,
+              headers: headers,
+              body: { }.to_json ).parsed_response
+  merkle_id = response['id'].to_s
 
-	# write merkel.id in item
-	update_item_url = config["pia_url"] + '/api/items/' + item_id.to_s
-	tmp = HTTParty.put(update_item_url,
-		    headers: headers,
-		    body: {
-          oyd_hash: item_hash.unpack('H*')[0].to_s,
-          merkle_id: merkle_id }.to_json )
+  # process each item
+  items.each do |item|
+      item_id = item['id']
+      item_hash = Digest::SHA256.digest(item['value'])
+      id_array << item_id
+      hash_array << item_hash
+      # puts item_id.to_s + ": " + Base64.encode64(item_hash).rstrip
+
+      # write merkel.id in item
+      update_item_url = config["pia_url"] + '/api/items/' + item_id.to_s
+      tmp = HTTParty.put(update_item_url,
+              headers: headers,
+              body: {
+                oyd_hash: item_hash.unpack('H*')[0].to_s,
+                merkle_id: merkle_id }.to_json )
+
+  end
+
+  if hash_array.length == 1
+      serialized_object = ""
+      root_node = hash_array.first.unpack('H*')[0]
+  else 
+      serialized_object = Base64.strict_encode64(Marshal::dump(mht)).strip
+      root_node = mht.head().unpack('H*')[0]
+  end
+  # puts "===================\n"
+  # puts "root_node: " + root_node.to_s
+
+  case ENV["BLOCKCHAIN"].to_s
+  when "ARTIS"
+      blockchain_url = 'http://' + ENV["DOCKER_LINK_BC"].to_s + ':' + (ENV["DOCKER_LINK_BC_PORT"] || "3000") + '/api/create'
+      response = HTTParty.get(blockchain_url + "?hash=0x" + root_node)
+      oyd_transaction = response.parsed_response["tx"].to_s
+  else
+      # request transaction
+      blockchain_url = 'http://' + ENV["DOCKER_LINK_BC"].to_s + ':' + (ENV["DOCKER_LINK_BC_PORT"] || "4510") + '/create'
+      puts "blockchain_url: " + blockchain_url.to_s
+      puts "merkle_id: " + merkle_id.to_s
+      puts "root_node: " + root_node.to_s
+      response = HTTParty.post(blockchain_url,
+                      headers: { 'Content-Type' => 'application/json'},
+                      body: { id:   merkle_id, 
+                              hash: root_node }.to_json ).parsed_response
+      # puts "repsonse: " + response.to_s
+      oyd_transaction = response['transaction-id']
+  end
+
+  # update merkel record and store
+  update_merkle_url = config["pia_url"] + '/api/merkles/' + merkle_id.to_s
+  response = HTTParty.put(update_merkle_url,
+              headers: headers,
+              body: { payload:         id_array.to_json,
+                      merkle_tree:     serialized_object,
+                      root_hash:       root_node,
+                      oyd_transaction: oyd_transaction }.to_json )
+
+  # log entry
+  writeLog(ma, 'wrote ' + root_node.to_s + ' at ' + oyd_transaction.to_s)
 
 end
-serialized_object = Base64.encode64(Marshal::dump(mht)).strip
-root_node = mht.head().unpack('H*')[0]
-
-# puts "===================\n"
-# puts "root_node: " + root_node.to_s
-
-# request transaction
-blockchain_url = 'http://' + ENV["DOCKER_LINK_BC"].to_s + ':3010/create'
-# puts "blockchain_url: " + blockchain_url.to_s
-response = HTTParty.post(blockchain_url,
-	            headers: { 'Content-Type' => 'application/json'},
-	            body: { id:   merkle_id, 
-	                    hash: root_node }.to_json ).parsed_response
-# puts "repsonse: " + response.to_s
-oyd_transaction = response['transaction-id']
-
-# update merkel record and store
-update_merkle_url = config["pia_url"] + '/api/merkles/' + merkle_id.to_s
-response = HTTParty.put(update_merkle_url,
-		    headers: headers,
-		    body: { payload:     id_array.to_json,
-		    		merkle_tree:     serialized_object,
-		    		root_hash:       root_node,
-		    		oyd_transaction: oyd_transaction }.to_json )
-
-# log entry
-writeLog(ma, 'wrote ' + root_node.to_s + ' at ' + oyd_transaction.to_s)
 
 # =============
 # verify record
